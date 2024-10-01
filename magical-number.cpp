02fb095c96eb2f4aec90c804977f94cd9f8c737c
JasimDAce <mohdalijasim786@gmail.com>
2024-10-02 02:37:27 +0530

    I have solved nqueen in space complexity of (n2)

--- magical-number.cpp
+++ magical-number.cpp
@@ -4,21 +4,6 @@
 using namespace std;
 
 int main(){
-    int n; cin >> n;
-    int count =0 ;
-    for(int i = 1; i <= n ;i++){
-        int sum = 0;
-        int j = i;
-        while(j){
-            if(j&1)
-                sum += 2;
-            else
-                sum += 1;
-            j >>=1;
-        }
-        if(sum%2 == 1)
-            count++;
-    }
-    cout << count;
+    cout << "Hello World Inside Vim";
     return 0;
 }
---
+++ nqueenUsingMap.cpp
@@ -0,0 +1,37 @@
+class Solution{
+public:
+	void f(int col,int n , vector<vector<int>> &ans, vector<vector<int>> & board , vector<int> & prevRow, vector<int>& upperDiagonal, vector<int>& lowerDiagonal){
+	if(col == n){
+		vector<int> ds;
+		for(auto a : board){
+			for(auto b: a){
+				ds.push_back(b);
+			}
+		}
+	}
+
+	for(int row = 0; row < n;row++){
+		if(prevRow[row] == 0 && upperDiagonal[n-1+col-row] == 0 && lowerDiagonal[row+col] == 0){
+			prevRow[row] = 1;
+			upperDiagonal[n-1+col-row] = 1;
+			lowerDiagonal[row+col] = 1;
+			board[row][col] = 1;
+			f(col+1, n , ans , board, preRow ,upperDiagonal, lowerDiagonal);
+			preRow[row] = 0;
+			upperDiagonal[n-1+col-row] = 0;
+			lowerDiagonal[row+col]= 1;
+			board[row][col] = 0;
+		}
+	}
+
+}
+    vector<vector<int>> nQueen(int n) {
+    	vector<vector<int>> ans;
+	vector<vector<int>> board(n , vector<int> (n, 0));
+	vector<int>prevRow(n,0);
+	vector<int>upperDiagonal(n*2-1,0);
+	vector<int>lowerDiagonal(n*2-1,0);
+	f(0,n , ans , board , prevRow, upperDiagonal, lowerDiagonal);	
+	return ans;	
+    }
+};
