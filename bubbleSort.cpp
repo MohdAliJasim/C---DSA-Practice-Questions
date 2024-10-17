4c82f02f2da0a793412127119e7d3a2fe8f59f97
JasimDAce <mohdalijasim786@gmail.com>
2024-10-17 23:11:38 +0530

    I have solved bubbleSort

---
+++ bubbleSort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+  int n; 
+  vector<int> arr;
+  int num;
+  while(cin >> num) arr.push_back(num);
+  for(int i = 0 ;i < arr.size() ;i++){
+    for(int j = 0; j < arr.size()-i-1;j++){
+      if(arr[j] > arr[j+1]) swap(arr[j], arr[j+1]);
+    }
+  }
+  for(auto a : arr) cout << a << " ";
+}
