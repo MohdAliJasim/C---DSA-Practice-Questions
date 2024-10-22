f5ebffeb891a9909a2b815bedcba307fbe399e1c
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-10-22 13:47:49 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 11.50%] - LeetPush

--- 198-House-Robber.cpp
+++ 198-House-Robber.cpp
@@ -1,21 +1,54 @@
 class Solution {
 public:
-    int fn(vector<int>& nums, int index, vector<int> & dp){
-        if(index >= nums.size()) return 0;
+// Memoization
+    // int fn(vector<int>& nums, int index, vector<int> & dp){
+    //     if(index >= nums.size()) return 0;
 
-        if(dp[index] != -1) return dp[index];
+    //     if(dp[index] != -1) return dp[index];
         
-        int pick = fn(nums, index+2, dp) + nums[index];
+    //     int pick = fn(nums, index+2, dp) + nums[index];
 
-        int notPick = fn(nums, index+1,dp) + 0;
+    //     int notPick = fn(nums, index+1,dp) + 0;
 
-        dp[index] = max(pick ,notPick);
-        return dp[index];
+    //     dp[index] = max(pick ,notPick);
+    //     return dp[index];
 
-    }
+    //  int rob(vector<int>& nums) {
+    //     vector<int> dp(nums.size()+1 , -1);
+    //     return fn(nums, 0, dp);
+    // }
+    // }
 
+
+//Tabulation
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size()+1 , -1);
-        return fn(nums, 0, dp);
+        vector<int> dp(nums.size());
+        dp[0] = nums[0];
+        int neg = 0;
+        for(int i = 1 ;i < nums.size();i++){
+            int pick = nums[i]; if(i > 1) pick += dp[i-2];
+            int notPick = 0 + dp[i-1];
+            dp[i] = max(pick , notPick);
+        }
+        return dp[nums.size()-1];
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
