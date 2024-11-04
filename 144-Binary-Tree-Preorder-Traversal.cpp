1ea8803ada7628c2c21574ce23c56b73b8c827fe
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-11-05 04:42:33 +0530

    [144] [Time Beats: 100.00%] [Memory Beats: 14.68%] - LeetPush

--- 144-Binary-Tree-Preorder-Traversal.cpp
+++ 144-Binary-Tree-Preorder-Traversal.cpp
@@ -11,16 +11,18 @@
  */
 class Solution {
 public:
-    void pre(TreeNode* root , vector<int>& res){
-        if(!root) return ;
-
-        res.push_back(root->val);
-        pre(root->left, res);
-        pre(root->right,res);
-    }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> res;
-        pre(root,res);
+        vector<int>res;
+        if(!root) return res;
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node = st.top();
+            st.pop();
+            res.push_back(node->val);
+            if(node->right) st.push(node->right);
+            if(node->left) st.push(node->left); 
+        }
         return res;
     }
 };
