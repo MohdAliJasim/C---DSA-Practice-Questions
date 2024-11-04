aadd03ca556fa4f1f769b3da155da0ed291db700
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-11-05 03:20:55 +0530

    [160] [Time Beats: 53.43%] [Memory Beats: 23.60%] - LeetPush

--- 160-Intersection-of-Two-Linked-Lists.cpp
+++ 160-Intersection-of-Two-Linked-Lists.cpp
@@ -11,21 +11,19 @@ public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
         ListNode* temp1 = headA;
         ListNode* temp2 = headB;
-        if(temp1 == nullptr || temp2 == nullptr) return nullptr;
 
-        while(temp1 != nullptr || temp2 != nullptr){
+        while(temp1 || temp2){
+             if(!temp1){
+                temp1 = headB;
+            }
+            if(!temp2){
+                temp2 = headA;
+            }
             if(temp1 == temp2) return temp1;
-
-
            
-            if(temp1 == nullptr) temp1 = headB;
-            else temp1 = temp1->next;
-
-            if(temp2 == nullptr) temp2 = headA;
-            else temp2 = temp2->next;
-
+            temp1 =temp1->next;
+            temp2 = temp2->next;
         }
-
 
         return nullptr;
     }
