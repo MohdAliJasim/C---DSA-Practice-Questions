82e78246ef5cc2bc318fd2ea78b4e858cddc2d8f
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-11-17 22:48:52 +0530

    [88] [Time Beats: 100.00%] [Memory Beats: 22.92%] - LeetPush

--- 88-Merge-Sorted-Array.cpp
+++ 88-Merge-Sorted-Array.cpp
@@ -1,32 +1,30 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i =0;
-        int j = 0;
-        vector<int> ans;
+        int i = 0, j = 0;
+        vector<int> nums3;
         while(i < m || j < n){
-            if(i < m && j < n){
+            if(i< m && j < n){
                 if(nums1[i] < nums2[j]){
-                    ans.push_back(nums1[i]);
+                    nums3.push_back(nums1[i]);
                     i++;
                 }
                 else{
-                    ans.push_back(nums2[j]);
+                    nums3.push_back(nums2[j]);
                     j++;
                 }
             }
-            else if(i < m){
-                ans.push_back(nums1[i]);
+            else if(i<m){
+                nums3.push_back(nums1[i]);
                 i++;
             }
             else{
-                ans.push_back(nums2[j]);
+                nums3.push_back(nums2[j]);
                 j++;
             }
-        }
+        }  
         nums1.clear();
-        for(int i = 0;i< ans.size();i++){
-            nums1.push_back(ans[i]);
-        }
+        nums1.insert(nums1.begin(),nums3.begin(),nums3.end());
+ 
     }
 };
