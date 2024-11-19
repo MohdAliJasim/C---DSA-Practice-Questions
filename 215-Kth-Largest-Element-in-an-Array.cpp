7ce52c1d4fef646e7113d78259004a858440668b
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-11-20 02:11:21 +0530

    [215] [Time Beats: 26.78%] [Memory Beats: 12.08%] - LeetPush

--- 215-Kth-Largest-Element-in-an-Array.cpp
+++ 215-Kth-Largest-Element-in-an-Array.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
     int findKthLargest(vector<int>& nums, int k) {
-        sort(nums.begin(), nums.end());
-        return nums[nums.size()-k];
+       priority_queue<int> q;
+       for(int i =0 ;i < nums.size();i++){
+        q.push(nums[i]);
+       }
+       int ans = 0;
+       for(int i =0;i < k;i++){
+            ans = q.top();
+            q.pop();
+       }
+       return ans;
     }
 };
