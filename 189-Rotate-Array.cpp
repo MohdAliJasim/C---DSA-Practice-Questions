e8d40eb2dd3e5ef8f2bf11f00dced2421f888ac9
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-11-22 18:20:31 +0530

    [189] [Time Beats: 100.00%] [Memory Beats: 80.99%] - LeetPush

--- 189-Rotate-Array.cpp
+++ 189-Rotate-Array.cpp
@@ -1,15 +1,9 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        
-        vector<int> ans;
         k = k%nums.size();
-        if(k == 0) return;
-        for(int i =0;i< nums.size();i++){
-           
-            ans.push_back(nums[(nums.size()+i-k)%nums.size()]);
-        }
-        nums.clear();
-        nums.insert(nums.begin(),ans.begin(),ans.end());
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(), nums.begin()+k);
+        reverse(nums.begin()+k, nums.end());
     }
 };
