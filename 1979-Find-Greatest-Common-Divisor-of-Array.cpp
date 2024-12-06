3c7e87155ccaf80a5c6ac8ee4718327b0cb67f3f
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-12-06 20:42:30 +0530

    [1979] [Time Beats: 100.00%] [Memory Beats: 6.37%] - LeetPush

--- 1979-Find-Greatest-Common-Divisor-of-Array.cpp
+++ 1979-Find-Greatest-Common-Divisor-of-Array.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
+    int gcd(int a , int b){
+        if(a == 0) return b;
+        if(b == 0) return a;
+
+        if(a > b){
+            return gcd(a%b,b);
+        }
+        else return gcd(b%a,a);
+    }
     int findGCD(vector<int>& nums) {
-        int mini = nums[0];
-        int maxi = nums[0];
-        for(int a : nums){
-            maxi = max(maxi , a);
-            mini = min(mini, a);
-        }
-        while(mini != 0 && maxi != 0){
-            if(mini > maxi) mini = mini%maxi;
-            else maxi = maxi%mini;
-        }
-        if(maxi == 0) return mini;
-        else return maxi;
+       int mini = INT_MAX;
+       int maxi  = INT_MIN;
+
+       for(auto a : nums){
+        mini = min(mini , a);
+        maxi = max(maxi, a);
+       }
+        return gcd(mini, maxi);
     }
 };
