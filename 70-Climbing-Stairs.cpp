5e898fcaec978e18a7c43a5f93f870165b5c56a6
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-12-14 12:11:18 +0530

    [70] [Time Beats: 100.00%] [Memory Beats: 12.71%] - LeetPush

--- 70-Climbing-Stairs.cpp
+++ 70-Climbing-Stairs.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
-    // int cs(int n , vector<int>& ds){
-    //      if(n == 0 || n == 1){
-    //         return 1;
-    //     }
-    //     if(n < 0) return 0;
-    //     if(ds[n] != -1) return ds[n];
-        
-    //     ds[n] = cs(n-1,ds) + cs(n-2,ds);
-    //     return ds[n];
-    // }
     int climbStairs(int n) {
-        vector<int> ds(n+1);
-        // ds[0] = 1;
-        // ds[1] = 1;
-        int prev1 = 1;
-        int prev2 = 1;
-        for(int i = 2 ; i<= n; i++){
-            //ds[i] = ds[i-1] + ds[i-2];
-            int curr = prev1 + prev2;
-            prev2 = prev1;
-            prev1 = curr;
+        if(n == 0 || n == 1) return n;
+        vector<int>dp(n+1);
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for(int i = 3; i < n+1; i++){
+            dp[i] = dp[i-2] + dp[i-1];
         }
-        return prev1;
-       // return ds[n];
-      //  return cs(n,ds);
+
+        return dp[n];
     }
 };
