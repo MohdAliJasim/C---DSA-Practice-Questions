28c15c4c3c317c161d7762d5ada4d12f18d0d66f
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-12-14 08:51:40 +0530

    [198] [Time Beats: 100.00%] [Memory Beats: 13.11%] - LeetPush

--- 198-House-Robber.cpp
+++ 198-House-Robber.cpp
@@ -1,67 +1,14 @@
 class Solution {
 public:
-// Memoization
-    // int fn(vector<int>& nums, int index, vector<int> & dp){
-    //     if(index >= nums.size()) return 0;
 
-    //     if(dp[index] != -1) return dp[index];
-        
-    //     int pick = fn(nums, index+2, dp) + nums[index];
-
-    //     int notPick = fn(nums, index+1,dp) + 0;
-
-    //     dp[index] = max(pick ,notPick);
-    //     return dp[index];
-
-    //  int rob(vector<int>& nums) {
-    //     vector<int> dp(nums.size()+1 , -1);
-    //     return fn(nums, 0, dp);
-    // }
-    // }
-
-
-//Tabulation
-//     int rob(vector<int>& nums) {
-//         vector<int> dp(nums.size());
-//         dp[0] = nums[0];
-//         int neg = 0;
-//         for(int i = 1 ;i < nums.size();i++){
-//             int pick = nums[i]; if(i > 1) pick += dp[i-2];
-//             int notPick = 0 + dp[i-1];
-//             dp[i] = max(pick , notPick);
-//         }
-//         return dp[nums.size()-1];
-//     }
-//
-
-//Space Optimization
-
-int rob(vector<int>& nums){
-    int prev = nums[0];
-    int beforePrev = 0;
-    for(int i =1 ;i < nums.size();i++){
-        int pick = nums[i] + beforePrev;
-        int notPick = prev;
-        int curr = max(pick , notPick);
-        beforePrev = prev;
-        prev = curr;
+    int solve(vector<int>& nums, int n,vector<int>& dp){
+        if(n < 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = max(nums[n] + solve(nums,n-2,dp),solve(nums,n-1,dp));
     }
-    return prev;
-}
 
- };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    int rob(vector<int>& nums) {
+        vector<int> dp(nums.size()+1,-1);
+        return solve(nums,nums.size()-1,dp);
+    }
+};
