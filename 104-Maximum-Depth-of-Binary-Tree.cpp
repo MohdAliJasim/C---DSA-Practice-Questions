f948fe49c7a271bab6f2b6bf07415c708e43054a
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-12-26 23:14:00 +0530

    [104] [Time Beats: 100.00%] [Memory Beats: 44.27%] - LeetPush

--- 104-Maximum-Depth-of-Binary-Tree.cpp
+++ 104-Maximum-Depth-of-Binary-Tree.cpp
@@ -14,9 +14,6 @@ public:
     int maxDepth(TreeNode* root) {
         if(!root) return 0;
 
-        int left = maxDepth(root->left);
-        int right = maxDepth(root->right);
-
-        return max(left,right) +1;
+        return max(maxDepth(root->left), maxDepth(root->right)) + 1;
     }
 };
