a028a6378e9d8761569457865b0e47cc1aff3289
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-12-29 07:20:30 +0530

    [1051] [Time Beats: 9.90%] [Memory Beats: 13.19%] - LeetPush

---
+++ 1051-Height-Checker.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> arr = heights;
+        sort(heights.begin(), heights.end());
+        int cnt = 0;
+        for(int i = 0 ;i < arr.size();i++){
+            if(arr[i] != heights[i]) cnt++;
+        }
+        return cnt;
+    }
+};
