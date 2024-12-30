5eb90b17b077132fdb300837bc557a3cfecc5c82
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2024-12-30 17:07:35 +0530

    [904] [Time Beats: 78.56%] [Memory Beats: 60.97%] - LeetPush

--- 904-Fruit-Into-Baskets.cpp
+++ 904-Fruit-Into-Baskets.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int maxLen =0;
-        int l =0;
-        int r = 0;
-        unordered_map<int, int> map;
+        int r = 0 ; 
+        int l = 0 ;
+        int maxLen = 0;
+        unordered_map<int ,int> map;
         while(r < fruits.size()){
             map[fruits[r]]++;
-            if(map.size()< 3) maxLen = max(maxLen , r-l+1);
             r++;
-            if(map.size() > 2){
+            if(map.size() <= 2){
+                maxLen = max(maxLen , r-l);
+            }
+            else{
                 map[fruits[l]]--;
                 if(map[fruits[l]] == 0){
                     map.erase(fruits[l]);
