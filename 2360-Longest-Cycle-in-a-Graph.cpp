d26dae7a2e1c99393e9d0eea8cb612b0d5e1d050
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2025-01-25 00:05:20 +0530

    [2360] [Time Beats: 64.38%] [Memory Beats: 83.23%] - LeetPush

--- 2360-Longest-Cycle-in-a-Graph.cpp
+++ 2360-Longest-Cycle-in-a-Graph.cpp
@@ -1,32 +1,38 @@
 class Solution {
 public:
-    int longestCycleLn = -1;
-    void dfs(vector<int>& edges, vector<bool> &visited, vector<int>& cp, int source , int len){
+    int maxi = -1;
+
+    void dfs(vector<int>& edges, vector<bool> &visited, vector<int> &cp, int source , int len){
         len++;
-        visited[source] =true;
+        visited[source] = true;
         cp[source] = len;
         int nbr = edges[source];
-        if(nbr != -1){
-            if(!visited[nbr]){
-                dfs(edges,visited,cp,nbr,len);
-            }
-            else if(cp[nbr] != 0){
-                    int newLn = cp[source]- cp[nbr] + 1;
-                    longestCycleLn = max(longestCycleLn , newLn);
-                }
+        if(nbr == -1){
+            cp[source] = 0;
+            len--;
+            return;
         }
-        
+        if(!visited[nbr]){
+            dfs(edges, visited, cp ,nbr , len);
+        }
+        else if(cp[nbr] != 0){
+            int mlen = len - cp[nbr] +1;
+            maxi = max(mlen  , maxi);
+        }
         cp[source] = 0;
+        len--;
     }
+
     int longestCycle(vector<int>& edges) {
         int n = edges.size();
+        vector<int>cp(n,0);
         vector<bool> visited(n,false);
-        vector<int> cp(n,0);
-        for(int i =0 ;i < n;i++){
+
+        for(int i =0;i <n ;i++){
             if(!visited[i]){
-                dfs(edges, visited, cp, i , 0);
+                dfs(edges, visited, cp, i,0);
             }
         }
-        return longestCycleLn;
+        return maxi;
     }
 };
