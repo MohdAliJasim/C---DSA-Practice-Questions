f8fe52932563c74d50bd10c73b9f77557d9a5743
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2025-01-31 21:40:39 +0530

    [2091] [Time Beats: 100.00%] [Memory Beats: 86.00%] - LeetPush

--- 2091-Removing-Minimum-and-Maximum-From-Array.cpp
+++ 2091-Removing-Minimum-and-Maximum-From-Array.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
     int minimumDeletions(vector<int>& nums) {
-        int n = nums.size();
-        if (n == 1) return 1;  // If there's only one element, only one deletion is needed.
-
-        // Finding indices of the minimum and maximum elements
-        int maxIdx = 0, minIdx = 0;
-        for (int i = 1; i < n; i++) {
-            if (nums[i] > nums[maxIdx]) maxIdx = i;
-            if (nums[i] < nums[minIdx]) minIdx = i;
+        int mini = 0;
+        int maxi = 0;
+        for(int i = 0;i < nums.size();i++){
+            if(nums[mini] > nums[i]){
+                mini = i;
+            }
+            if(nums[maxi] < nums[i]) maxi = i;
         }
 
-        // Ensure minIdx is always smaller than maxIdx for easier calculations later
-        if (minIdx > maxIdx) swap(minIdx, maxIdx);
+        int ini = min (maxi, mini);
+        int last = max(maxi , mini);
+        int n = nums.size();
+        int ans1 = last + 1;
+        int ans2 = n-ini;
+        int ans3 = (ini + 1) + (n - last);
 
-        // Calculate three possible strategies for deletions:
-        int deleteFromFront = maxIdx + 1;             // Removing both elements from the front
-        int deleteFromBack = n - minIdx;              // Removing both elements from the back
-        int deleteFromBothSides = minIdx + 1 + (n - maxIdx); // Removing min from the front and max from the back
-
-        // Return the minimum deletions among the three strategies
-        return min({deleteFromFront, deleteFromBack, deleteFromBothSides});
+        return min(ans1, min(ans2,ans3));
     }
 };
