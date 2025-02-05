83518e5ec81c59da767a0bbfa48b44a6622f1915
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2025-02-05 13:45:37 +0530

    [1790] [Time Beats: 100.00%] [Memory Beats: 9.35%] - LeetPush

--- DCP-02-25/1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.cpp
+++ DCP-02-25/1790-Check-if-One-String-Swap-Can-Make-Strings-Equal.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-        if(s1 == s2) return true;
-        for(int i = 0;i < s1.size();i++){
-            for(int j = i+1;j< s1.size();j++){
-                swap(s2[i], s2[j]);
-                if(s1 == s2) return true;
-                swap(s2[i], s2[j]);
-            }
+       unordered_map<char,int> map1;
+       unordered_map<char, int> map2;
+        int count = 0;
+        for(int i= 0 ;i < s1.size();i++){
+            if(s1[i] != s2[i]) count++;
+            map1[s1[i]]++;
+            map2[s2[i]]++;
         }
+        if(map1 != map2) return false;
+        if(count <= 2) return true;
+    
         return false;
     }
 };
