804161dd290e3bd2f089cf05f23edac9974216b8
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2025-02-16 23:05:50 +0530

    [206] [Time Beats: 100.00%] [Memory Beats: 40.49%] - LeetPush

--- 206-Reverse-Linked-List.cpp
+++ 206-Reverse-Linked-List.cpp
@@ -11,19 +11,17 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        vector<int> arr;
-        ListNode* temp = head;
-        while(temp){
-            arr.push_back(temp->val);
-            temp = temp->next;
+        
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* front = NULL;
+
+        while(curr){
+            front = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = front;
         }
-        temp = head;
-        int i = arr.size()-1;
-        while(temp){
-            temp->val = arr[i];
-            i--;
-            temp = temp->next;
-        }
-        return head;
+        return prev;
     }
 };
