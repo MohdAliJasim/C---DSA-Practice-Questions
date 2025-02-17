983c8cbc069e8e5c24d28901777fae241be7e79e
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2025-02-17 17:13:04 +0530

    [51] [Time Beats: 64.97%] [Memory Beats: 92.75%] - LeetPush

--- 51-N-Queens.cpp
+++ 51-N-Queens.cpp
@@ -1,43 +1,62 @@
 class Solution {
 public:
 
+    bool safe(vector<string>& temp, int row , int col){
+        int r = row;
+        int c = col;
 
-    void solve(int n , int col , vector<vector<string>>& res, vector<int>& upd, vector<int>& lowd, vector<int>& rowd,vector<string>&board){
-        if(col == n){
-           res.push_back(board);
-
-
-            return; 
+        while(r >= 0){
+            if(temp[r][c] == 'Q') return false;
+            r--;
         }
+        r = row;
+        while(c >= 0){
+            if(temp[r][c] == 'Q') return false;
+            c--;
+        }
+        c =col;
+        while(r >= 0 && c >= 0){
+            if(temp[r][c] == 'Q') return false;
+            c--;
+            r--;
+        }
+        c = col;
+        r  = row;
+        while(r >= 0 && c < temp.size()){
+            if(temp[r][c] == 'Q') return false;
+            c++;
+            r--;
+        }
+        return true;
+    }
 
-        for(int row= 0; row< n;row++){
-            if(upd[n-1+col-row] == 0 && lowd[col+row] == 0 && !rowd[row]){
-                upd[n-1+col-row] = 1;
-                lowd[col+row] = 1;
-                rowd[row] = 1;
-                board[row][col] = 'Q';
-                solve(n ,col+1,res,upd,lowd,rowd,board);
-                board[row][col] = '.';
-                upd[n-1+col-row] = 0;
-                lowd[col+row] = 0;
-                rowd[row] = 0;
-                
+
+    void solve(int n , int row , vector<vector<string>>& ans, vector<string>& temp){
+        if(row == n ){
+            ans.push_back(temp);
+            return;
+        }
+       
+
+        for(int i = 0; i < n;i++){
+            if(safe(temp,row,i)){
+                temp[row][i] = 'Q';
+                solve(n , row+1, ans , temp);
+                temp[row][i] = '.';
             }
         }
-      
     }
+
     vector<vector<string>> solveNQueens(int n) {
-        vector<vector<string>>res;
-        vector<string>board(n);
-        string s(n,'.');
-       
-        for(int i =0 ; i< n;i++){
-            board[i] = s;
-        }
-        vector<int>upd((n*2)-1,0);
-        vector<int>lowd((n*2)-1,0);
-        vector<int>row(n,0);
-        solve(n, 0 , res,upd,lowd,row,board);
-        return res;
+         vector<vector<string>> ans;
+         vector<string> temp(n);
+         for(int i = 0 ; i < n;i++){
+            for(int j = 0 ;j < n;j++){
+                temp[i].push_back('.');
+            }
+         }
+        solve(n , 0, ans , temp);
+        
+         return ans;
     }
 };
