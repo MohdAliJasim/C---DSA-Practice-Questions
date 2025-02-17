1340f3f6e1e55d77852121e576831f09f33b36c4
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2025-02-17 20:18:05 +0530

    [268] [Time Beats: 100.00%] [Memory Beats: 70.79%] - LeetPush

--- 268-Missing-Number.cpp
+++ 268-Missing-Number.cpp
@@ -1,21 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        for(int i= 0; i< nums.size()-1;i++){
-            bool flag = false;
-            if(nums[i] == i) continue;
-            
-            for(int j= i+1;j < nums.size();j++){
-                if(i == nums[j]){
-                    swap(nums[i],nums[j]);
-                    flag = true;
-                    break;
-                }
-            }
-           // if(nums[i] == nums.size()) continue;
-            if(flag == false) return i;
+        int n = nums.size();
+        int totalsum = n*(n+1)/2;
+        int sum = 0 ;
+        for(int i=0 ;i < nums.size();i++){
+            sum += nums[i];
         }
-        if(nums[nums.size()-1] != nums.size()-1) return nums.size()-1;
-        return nums.size();
-    }
+        return totalsum-sum;
+    }   
 };
