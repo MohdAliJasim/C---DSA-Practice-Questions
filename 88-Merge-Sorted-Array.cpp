01695fa6008aaf13dc3bcf81a7adb5f2d79741f5
Mohd Ali Jasim <143309851+MohdAliJasim@users.noreply.github.com>
2025-02-17 19:23:55 +0530

    [88] [Time Beats: 100.00%] [Memory Beats: 39.52%] - LeetPush

--- 88-Merge-Sorted-Array.cpp
+++ 88-Merge-Sorted-Array.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = 0, j = 0;
-        vector<int> nums3;
-        while(i < m || j < n){
-            if(i< m && j < n){
-                if(nums1[i] < nums2[j]){
-                    nums3.push_back(nums1[i]);
-                    i++;
-                }
-                else{
-                    nums3.push_back(nums2[j]);
-                    j++;
-                }
-            }
-            else if(i<m){
-                nums3.push_back(nums1[i]);
-                i++;
-            }
-            else{
-                nums3.push_back(nums2[j]);
-                j++;
-            }
-        }  
-        nums1.clear();
-        nums1.insert(nums1.begin(),nums3.begin(),nums3.end());
- 
-    }
+        int i  = m+n -1;
+        m = m-1;
+        n = n-1;
+        cout << i;
+        while(i >= 0){
+            if(m>= 0 && n >=0){
+                if(nums1[m] > nums2[n])   nums1[i--] = nums1[m--];          
+                else  nums1[i--] = nums2[n--];
+            }   
+            else if(m >= 0) nums1[i--] = nums1[m--];
+
+            else nums1[i--] = nums2[n--];  
+        }
+    }   
 };
